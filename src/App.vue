c85106c125566c1fe18e215828c7ef8cb50ade35
Nicolas Mermoud <nicolas.mermoud@textandbytes.com>
2022-05-23 22:16:45 +0200

    Improve app layout and styles

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
+  <div class="h-full flex flex-col justify-between border-2 border-gray-500 bg-white">
     <header class="bg-gray-100 p-16 flex items-start space-x-16 grow-0 text-gray-600">
       <RouterLink to="/"><img alt="Sci-Gate Logo" class="logo" src="@/assets/logo.svg" width="180" height="100" /></RouterLink>
       <SearchBar />
@@ -11,10 +12,14 @@ import { RouterLink, RouterView } from 'vue-router'
     <main class="grow">
       <RouterView />
     </main>
+  </div>
 </template>
 
 <style>
+body {
+  @apply bg-gray-300 min-h-screen;
+}
 #app {
-  @apply container mx-auto mt-12 p-8 border-2 border-gray-600;
+  @apply container h-full mx-auto my-12;
 }
 </style>
