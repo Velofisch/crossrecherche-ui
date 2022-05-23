84f2f0fa6bdc4580ec1adf3eb689188664e717e6
Nicolas Mermoud <nicolas.mermoud@textandbytes.com>
2022-05-23 09:50:11 +0200

    Add I18n plugin for Vue.js See: https://vue-i18n.intlify.dev/

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import { createApp } from 'vue'
+import { createI18n } from 'vue-i18n'
 import App from './App.vue'
 import router from './router'
 import './index.css'
 
+const i18n = createI18n()
+
 const app = createApp(App)
 
+app.use(i18n)
 app.use(router)
 
 app.mount('#app')
