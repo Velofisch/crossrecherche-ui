ca67106d4c04f905194d8a5b30da3e4db6d220e2
Nicolas Mermoud <nicolas.mermoud@textandbytes.com>
2022-05-23 22:12:34 +0200

    Improve header section styling. Add link to home page on logo.

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,9 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <img alt="Sci-Gate Logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-  </header>
+    <header class="bg-gray-100 p-16 flex items-start space-x-16 grow-0 text-gray-600">
+      <RouterLink to="/"><img alt="Sci-Gate Logo" class="logo" src="@/assets/logo.svg" width="180" height="100" /></RouterLink>
+    </header>
 
   <RouterView />
 </template>
