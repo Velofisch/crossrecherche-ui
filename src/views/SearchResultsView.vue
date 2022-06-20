c2be1aeb8fdc95c99f08d61c080586f0855e2f63
Nicolas Mermoud <nicolas.mermoud@textandbytes.com>
2022-06-20 18:25:57 +0200

    If there are no results for a specific search term and a specific search engine, display an appropriate message in the tab content. If there are more results loading, display a loading spinner and a "Loading more results..." message. When the end of the hitlist have been reached, the loading spinner is not displayed.

--- src/views/SearchResultsView.vue
+++ src/views/SearchResultsView.vue
@@ -69,6 +69,18 @@ function setObserver(id) {
         <TabContent v-for="searchEngine in searchEngines.filter(function(u) { return u.checked})" :key="'tab-content-' + searchEngine.id" :selected="selectedTab === searchEngine.id" :class="[{'active': selectedTab === searchEngine.id}]">
             <SearchResults v-if="searchEngine.hits>0" :id="searchEngine.id" :results="searchEngine.hitlist" @setObserver="setObserver"></SearchResults>
             <SearchResults v-else :id="searchEngine.id" :results="[]" @setObserver="setObserver"></SearchResults>
+            <div v-if="searchEngine.hits === 0">
+                <p>
+                    There is no result for the search term <span class="font-semibold">{{ searchEngine.searchterm }}</span> in this search engine.
+                </p>
+            </div>
+            <div v-else-if="searchEngine.hits > searchEngine.hitsLoaded" class="flex flex-col items-center">
+                <svg class="animate-spin p-4 h-20 w-20 text-gray-600 text-center" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                    <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                    <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                </svg>
+                <span class="text-sm">Loading more results...</span>
+            </div>
         </TabContent>
     </div>
 </template>
