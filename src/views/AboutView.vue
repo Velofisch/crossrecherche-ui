12e8aa8008d9caf531e59ec9a5a30ea40fe55679
Jörn Erbguth <joern@erbguth.net>
2023-04-30 07:54:53 +0200

    Update AboutView.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,13 +1,30 @@
 <template>
   <div class="p-16">
     <h1 class="font-bold">Über dieses Projekt</h1>
-    <p class="inline-block py-3">Wäre es nicht schön, von einem Ort aus das ganze Textkorpus „Recht“ durchsuchen zu können – alle Rechtsdaten, Gesetze, wissenschaftliche Veröffentlichungen, Urteile und sonstige Texte aus der sogenannten Rechtspraxis? Und wäre es nicht schön, auf all diese Rechtstexte dann auch sogleich zugreifen zu können?</p>
-	<p class="inline-block py-3">Mit just.online soll eine Schnittstelle zum Textkorpus Recht skizziert werden, die klassische Suchschnittstelle und moderne Programmierschnittstelle zugleich ist. Ziel von iust.online ist die Zurverfügungstellung einer One-Stop-Shop-Lösung für Rechtstexte bzw. Rechtsdaten im weitesten Sinne.</p>
-    <p class="inline-block py-3">iust.online ist, wenn überhaupt, ein „running prototype“, ein Konzept, das hoffentlich nicht zu langweilig ist, um diskutiert zu werden. Die Herausforderungen sind gross, doch die damit verbundene Möglichkeiten zu verlockend.</p>
-    <p class="inline-block py-3">Zur Reduzierung der Komplexität wird eine weitgehende Modularisierung von iust.online angestrebt. So erfolgt insbesondere die Datenaggregierung weiter an verschiedensten Orten, in verschiedensten Formaten und Datenbanken. Dreh- und Angelpunkt von iust.online werden damit sogenannte Plug-Ins, deren Aufgabe es ist, Datenquellen anzusprechen und ihre Antwort zurück für iust.online zu übersetzen.</p>
+    <p class="inline-block py-3">Künstliche Intelligenz braucht Daten, natürliche Intelligenz auch. Gateway zu diesen Daten im weitesten Sinne war früher, nicht zuletzt wenn es um die Wissenschaft ging, vor allem die Bibliothek. Gatekeeper zu diesen Daten waren die Bibliothekarinnen und Bibliothekare, die die Texte, die Daten mit Metadaten versahen und sie so such- und auffindbar machten.</p>
+    <p class="inline-block py-3">Von dieser Bibliothek ist heute wenig geblieben. Wissenschaftsdaten werden heute häufig in Datenbanken verschiedenster Akteure zur Verfügung gestellt, mit unterschiedlichem Zugang und verschiedenster Zugänglichkeit, nicht zuletzt in Hinblick auf die inhaltliche Erschliessung und so auch wiederum Such- und Auffindbarkeit der Daten.</p>
+    <p class="inline-block py-3">Diese Fragmentierung des Zugangs zu Wissenschaftsdaten beeinträchtigt die nationale und internationale Forschung und deren Sichtbarkeit. Das Projekt „scigate“ möchte versuchen, hier einen Gegenpol zu bilden. Jenseits der weiterhin bestehenden und zudem wünschenswerten Vielfalt der Datenquellen soll ein einheitlicher Sucheinstieg in die heute stark fragmentiere Landschaft wissenschaftlicher Datenbanken sowie ein niederschwellig zugänglicher One-Stop-Shop für Wissenschaftsdaten geschaffen werden.</p>
+    <p class="inline-block py-3">Mit scigate.online haben wir einen ersten Prototypen geschaffen, mit dem Schwerpunkt rechtswissenschaftliche Daten. Denn wäre es nicht schön, von einem Ort aus das ganze Textkorpus „Recht“ durchsuchen zu können – alle Rechtsdaten, Gesetze, wissenschaftliche Veröffentlichungen, Urteile und sonstige Texte aus der sogenannten Rechtspraxis? Und wäre es nicht mindestens ebenso schön, auf all diese Rechtstexte dann auch sogleich zugreifen zu können?</p>
+    <p class="inline-block py-3">Mit scigate.online soll eine solche Schnittstelle zum Textkorpus Recht skizziert werden, die klassische Suchschnittstelle und moderne Programmierschnittstelle zugleich ist.</p>
+    <p class="inline-block py-3">scigate.online ist, wenn überhaupt, ein „running prototype“, ein Konzept, das hoffentlich nicht zu langweilig ist, um diskutiert zu werden. Die Herausforderungen sind gross, doch die damit verbundene Möglichkeiten zu verlockend.</p>
+    <p class="inline-block py-3">Zur Reduzierung der Komplexität wird eine weitgehende Modularisierung von scigate.online angestrebt. So erfolgt insbesondere die Datenaggregierung weiter an verschiedensten Orten, in verschiedensten Formaten und Datenbanken. Dreh- und Angelpunkt von scigate.online werden damit sogenannte Plug-Ins, deren Aufgabe es ist, Datenquellen anzusprechen und ihre Antwort zurück für scigate.online zu übersetzen.</p>
     <p class="inline-block py-3">Wie aber werden dann etwa die Suchergebnisse dargestellt? Kann man diese so heterogenen Suchergebnisse in einer Liste zusammenführen? Inwieweit müssen wir die Daten der einzelnen Datenquellen homogenisieren, um sie dann auch homogen abrufen zu können? Wie gehen wir mit Dubletten um? Wie gewichtet man die Ergebnisse? Legt man die Gewichtung offen? Soll man dann gar zwischen verschiedensten offenen Gewichtungen und Suchalgorhythmen wählen können? Hilft eine graphische Aufarbeitung der Suchergebnisse? Und wie steht man zur Künstlichen Intelligenz, als Abbild dieses so bereits geschaffenen Wissens und als vielleicht Motor künftiger Wissenschaft? Eine Lösung für alles auf einmal wird man kaum finden. Aber auf zweimal, dreimal, Stück für Stück und modular. Vielleicht.</p>
-    <p class="inline-block py-3">Mit iust.online wird die Sichtbarkeit offener Rechtsdaten erhöht und ihr Nutzen in dieser Gesamtheit offensichtlich. Mehr wert als geschlossene Rechtsdaten sind sie allemal.</p>
-    <p class="inline-block py-3">Offen gestellt ist schliesslich die Frage, wer für solche Rechtsdaten wem wieviel bezahlt. Diese Diskussion ist nicht zuletzt mit den heutigen juristischen Grossverlagen zu führen, die jenseits des Verlagshandwerks mit ihren Datenbanken die öffentlichen Bibliotheken als Gatekeeper des Rechtswissens weitestgehend abgelöst haben.</p>
+    <p class="inline-block py-3">Mit scigate.online wird die Sichtbarkeit offener Rechtsdaten erhöht und ihr Nutzen in dieser Gesamtheit offensichtlich. Offen gestellt ist damit aber auch die Frage, wer für solche Rechtsdaten wem wieviel bezahlt. Diese Diskussion ist nicht zuletzt mit den heutigen juristischen Grossverlagen zu führen, die jenseits des Verlagshandwerks mit ihren Datenbanken die öffentlichen Bibliotheken als Gatekeeper des Rechtswissens weitestgehend abgelöst haben.</p>
     <p class="inline-block py-3">Alles nicht so einfach?</p>
   </div>
 </template>
+
+ 
+
+ 
+
+
+
+
+
+
+
+
+
+
+
